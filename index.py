e04857e0960abb4fb35a9b5a141ef5cfc0c3d23f
loraccc <carolacharya1@gmail.com>
2023-08-13 12:22:36 +0545

    CAROL COMMITED

--- index.py
+++ index.py
@@ -1,3 +1,22 @@
 i= range(1,11)
 for a in i:
     print(a)
+    
+    
+a=[1,2,3,4,5]
+b=[1,2,3,4]
+if a[1]== b[-1]:
+    print('They re the same')
+else:
+    print('no theyre not the same')
+    
+    
+name=['carol','sushil','bibek','balram']
+for i in name:
+    if name == 'bibek':
+        break
+    print(i)
+    
+r=range(1,20)
+for i in r:
+    print(i)
