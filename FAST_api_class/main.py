2311a39390a7b02c2559949266f58a3ef64d9398
loraccc <carolacharya1@gmail.com>
2023-08-24 21:25:30 +0545

    postgre pgadmin crud Fastapi

--- FAST_api_class/main.py
+++ FAST_api_class/main.py
@@ -1,21 +1,52 @@
-from typing import Union
-from fastapi import FastAPI
-import employee
-from db_setup import SessionLocal, engine
-
-employee.Base.metadata.create_all(bind=engine)
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+import models, schemas, crud 
+from database import engine, get_db
 
 
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+# #MODELS 
+app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="user not found")
+    return db_user
+
+
+
+
+@app.post("/itemss/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    db_item = crud.get_item_by_title(db, title=item.title)
+    if db_item:
+        raise HTTPException(status_code=400, detail="item already exists")
+    return crud.create_item(db=db, item=item)
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
